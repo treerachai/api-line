b5d3b84530aa18f85d238c0506f166014d0af041
Farzain - zFz <32348150+farzain@users.noreply.github.com>
2017-10-22 16:45:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -179,45 +179,7 @@ if($message['type']=='text')
 				
 	}
 	
-	else
-	if($pesan_datang=='8')
-	{
-		
-		$balas = array(
-							'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'template',					
-										'altText' => 'this is a buttons template',
-										'template' => {
-											'type' => 'buttons',
-											'thumbnailImageUrl' => 'https://example.com/bot/images/image.jpg',
-											'title' => 'Menu',
-											'text' => 'Please select',
-											'actions' => [
-											{
-											  'type' => 'postback',
-											  'label' => 'Buy',
-											  'data' => 'action=buy&itemid=123'
-											},
-											{
-											  'type' => 'postback',
-											  'label' => 'Add to cart',
-											  'data' => 'action=add&itemid=123'
-											},
-											{
-											  'type' => 'uri',
-											  'label' => 'View detail',
-											  'uri' => 'http://google.com'
-											}
-										]
-									}
-								)
-							)
-						);
-						
-				
-	}
+
 
 	else{
 
