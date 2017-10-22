b19ffd3810e7a4330aac90f2ca58dc4b72e126cb
Farzain - zFz <32348150+farzain@users.noreply.github.com>
2017-10-22 16:30:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -178,6 +178,36 @@ if($message['type']=='text')
 		$client->pushMessage($push);
 				
 	}
+	
+	else
+	if($pesan_datang=='8')
+{
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
 
 	else{
 
