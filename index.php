c5363bc19375d57f84eb8c4741904ef994f2ec32
Farzain - zFz <32348150+farzain@users.noreply.github.com>
2017-10-22 16:34:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -181,29 +181,29 @@ if($message['type']=='text')
 	
 	else
 	if($pesan_datang=='8')
-{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
+	{
+  'type' => 'template',
+  'altText' => 'this is a buttons template',
+  'template' => {
+      'type' => 'buttons',
+      'thumbnailImageUrl' => 'https://medantechno.com/line/images/bolt/1000.jpg',
+      'title' => 'Menu',
+      'text' => 'Please select',
+      'actions' => [
           {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
+            'type' => 'postback',
+            'label' => 'Buy',
+            'data' => 'action=buy&itemid=123'
           },
           {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
+            'type' => 'postback',
+            'label' => 'Add to cart',
+            'data' => 'action=add&itemid=123'
           },
           {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
+            'type' => 'uri',
+            'label' => 'View detail',
+            'uri' => 'http://example.com/page/123'
           }
       ]
   }
